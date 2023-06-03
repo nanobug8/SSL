cc369132f1d236aad1785b44ebe7748d94eca848
andresrc92 <andresrivcoi@gmail.com>
2023-06-03 11:01:35 -0300

    agarra la pelota en toda la cancha

--- script/dummy_robot.py
+++ script/dummy_robot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 import rospy
 import numpy as np
@@ -11,10 +11,34 @@ from krssg_ssl_msgs.msg import *
 
 vision = SSL_DetectionFrame()
 
+ball_x = 0
+ball_y = 0
+robot_x = 0
+robot_y = 0
+robot_ang = 0
+
 def vision_callback(data):
     global vision
+    global ball_x
+    global ball_y
+    global robot_x
+    global robot_y
+    global robot_ang
+
     vision = data
-    print(data.camera_id)
+
+    if (len(vision.balls) > 0):
+        ball_x = vision.balls[0].x
+        ball_y = vision.balls[0].y
+        # print("Ball ", vision.balls[0].x, vision.balls[0].y, "\n")
+
+    for robot in vision.robots_blue:
+        if robot.robot_id == 0:
+            robot_x = robot.x
+            robot_y = robot.y
+            robot_ang = robot.orientation
+                # print("Robot ", robot_x, robot_y, robot_ang, "\n")
+    # print(data.camera_id)
 
 
 if __name__ == '__main__':
@@ -42,19 +66,6 @@ if __name__ == '__main__':
     state = 0
 
     while not rospy.is_shutdown():
-        if (len(vision.balls) > 0):
-            ball_x = vision.balls[0].x
-            ball_y = vision.balls[0].y
-            # print("Ball ", vision.balls[0].x, vision.balls[0].y)
-            # print("\n")
-
-
-        if (len(vision.robots_blue) > 0):
-            robot_x = vision.robots_blue[0].x
-            robot_y = vision.robots_blue[0].y
-            robot_ang = vision.robots_blue[0].orientation
-            # print("Robot ", vision.robots_blue[0].x, vision.robots_blue[0].y)
-            # print("\n")
 
         if np.abs(ball_y - robot_y) > 0:
             angle = np.arctan2((ball_y - robot_y), (ball_x - robot_x))
@@ -62,11 +73,11 @@ if __name__ == '__main__':
 
         # print(robot_ang)
         # print(angle)
-        # print(angle, robot_ang, angle - robot_ang)
+        print(angle, robot_ang, angle - robot_ang)
 
         if state == 0:
 
-            print("state ", state)
+            # print("state ", state)
             msg.dribbler = True
             msg.kicker = False
             
@@ -96,7 +107,7 @@ if __name__ == '__main__':
 
 
         elif state == 1:
-            print("state ", state)
+            # print("state ", state)
             msg.dribbler = True
             # print(np.random.rand(-3, 3))
             current_angle = robot_ang
@@ -105,7 +116,7 @@ if __name__ == '__main__':
 
         elif state == 2:
 
-            print("state ", robot_ang)
+            # print("state ", robot_ang)
             
             if current_angle > 0:
                 msg.cmd_vel.angular.z = - 0.7
@@ -118,12 +129,11 @@ if __name__ == '__main__':
                 state = 3
 
         elif state == 3:
-            print("state ", state)
+            # print("state ", state)
             msg.dribbler = False
             msg.kicker = True
             state = 0
                 
-
 
         ball_ok = False
         robot_ok = False
