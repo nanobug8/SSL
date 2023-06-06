f0cbfe61543adcb436aef9903fa1c5e548e278ff
andresrc92 <andresrivcoi@gmail.com>
2023-06-05 22:05:23 -0300

    changes to player.py

--- script/Player.py
+++ script/Player.py
@@ -34,32 +34,5 @@ class Player:
         print("Tirar al arco")
 
 
-<<<<<<< Updated upstream
-class Golero(Player):
-    def shortcut(self):
-        print("???")
-
-class Referi(Player):
-
-    golesAmarillo = 0
-    golesAzul = 0
-    def tanteador(self):
-        print(self.golesAmarillo," - ",self.golesAzul)
-
-
-    def is_yellow_goal(x1_y, y1_y, x2_y, y2_y, x3_y, y3_y, x4_y, y4_y):
-        if min(x1_y, x2_y, x3_y, x4_y) <= vision.balls[0].x <= max(x1_y, x2_y, x3_y, x4_y) and \
-                min(y1_y, y2_y, y3_y, y4_y) <= vision.balls[0].y<= max(y1_y, y2_y, y3_y, y4_y):
-            print("Goooooooooooooooooooooool Amarillo")
-
-
-    def is_blue_goal(x1_b, y1_b, x2_b, y2_b, x3_b, y3_b, x4_b, y4_b):
-        if min(x1_b, x2_b, x3_b, x4_b) <= vision.balls[0].x <= max(x1_b, x2_b, x3_b, x4_b) and \
-                min(y1_b, y2_b, y3_b, y4_b) <= vision.balls[0].y <= max(y1_b, y2_b, y3_b, y4_b):
-            print("Goooooooooooooooooooooool Azul")
-
-
-=======
->>>>>>> Stashed changes
 
 
