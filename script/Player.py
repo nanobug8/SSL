ca9446dc674aa111fd682e13678b21232625c26f
andresrc92 <andresrivcoi@gmail.com>
2023-06-09 00:03:04 -0300

    team players

--- script/Player.py
+++ script/Player.py
@@ -19,6 +19,7 @@ class Player:
         self.pub = rospy.Publisher(self.topic, SSL, queue_size = 10)
         self.vision_sub = rospy.Subscriber("/vision", SSL_DetectionFrame, self.vision_callback)
         self.id = id
+        self.team = team
         self.x = 0
         self.y = 0
         self.ball_x = 0
@@ -28,18 +29,23 @@ class Player:
         self.goal_x = 2000
         self.goal_y = 0
         self.goal_angle = 0
-        self.goal_dist = [0,0,0,0,0]
+        self.goal_dist = [1e4,1e4,1e4,1e4,1e4]
         self.mate_x = [0,0,0,0,0]
         self.mate_y = [0,0,0,0,0]
         self.mate_yaw = [0,0,0,0,0]
         self.mate_angle = [0,0,0,0,0]
-        self.mate_dist = [0,0,0,0,0]
+        self.mate_dist = [1e4,1e4,1e4,1e4,1e4]
         self.closest_mate = self.id
         self.closest_to_ball = self.id
         self.yaw = 0
-        self.angle_tolerance = 0.06
+        self.angle_tolerance = 0.12
         self.msg = SSL()
         self.state = 0
+        self.speed_scale = 0.3
+        self.ang_scale = 3
+        self.angle_right_post = 0
+        self.angle_left_post = 0
+
 
     def vision_callback(self,data):
 
@@ -48,7 +54,12 @@ class Player:
             self.ball_y = data.balls[0].y
             # print("Ball ", vision.balls[0].x, vision.balls[0].y, "\n")
 
-        for robot in data.robots_blue:
+        if self.team == "yellow":
+            robots = data.robots_yellow
+        else:
+            robots = data.robots_blue
+
+        for robot in robots:
             self.mate_x[robot.robot_id] = robot.x
             self.mate_y[robot.robot_id] = robot.y
             self.mate_yaw[robot.robot_id] = robot.orientation
@@ -64,8 +75,14 @@ class Player:
         self.y = self.mate_y[self.id]
         self.yaw = self.mate_yaw[self.id]
 
+        #Who are the closest?
         self.closest_mate = self.mate_dist.index(min(self.mate_dist))
         self.closest_to_ball = self.ball_dist.index(min(self.ball_dist))
+        self.closest_to_goal = self.goal_dist.index(min(self.goal_dist))
+
+        #Angle range to goal
+        self.angle_right_post = np.arctan2((self.goal_y - 500 - self.y), (self.goal_x - self.x))
+        self.angle_left_post = np.arctan2((self.goal_y + 500 - self.y), (self.goal_x - self.x))
 
         if np.abs(self.ball_y - self.y) > 0:
             self.ball_angle = np.arctan2((self.ball_y - self.y), (self.ball_x - self.x))
@@ -77,36 +94,72 @@ class Player:
 
 
         #Comportamental control
-        # if self.closest_to_ball == self.id:
-        #     self.state = 0
-        # else:
-        #     self.state = 2
-
-        if self.state == 0:
-            self.get_the_ball()
-        elif self.state == 1:
-            self.drive_to_goal()
-            # self.pass_to_mate()
-        elif self.state == 2:
+        if self.closest_to_ball == self.id:
+            
+            if self.state == 0:
+                print(self.id, " yendo \n")
+                self.speed_scale = 0.8
+                self.get_the_ball()
+            elif self.state == 1:
+                if self.ball_dist[self.id] > 300:
+                    print(self.id, " trancado \n")
+                    self.state = 0
+                    self.stop()
+                else:
+                    self.speed_scale = 0.5
+                    if self.goal_dist[self.id] - self.goal_dist[self.closest_to_goal] > 100 and self.closest_to_goal != self.id:
+                        print(self.id, " pasando \n")
+                        self.pass_to_mate()
+                    else:
+                        print(self.id, " chutando \n")
+                        self.drive_to_goal()
+                        if self.ball_dist[self.id] > 120:
+                            self.state = 0
+            # self.stop()
+        else:
+            self.speed_scale = 0.7
+            self.state = 0
             self.move_without_ball()
+            
+        if self.goal():
+            self.stop()
 
+        # if self.state == 0:
+        #     self.get_the_ball()
+        # elif self.state == 1:
+        #     self.drive_to_goal()
+        #     # self.pass_to_mate()
+        # elif self.state == 2:
+        #     self.move_without_ball()
+
+    def stop(self):
+        self.pub.publish()
+
+    def goal(self):
+        if self.ball_y < 500 and self.ball_y > -500 and self.ball_x > 2000:
+            return True
+        else:
+            return False
 
     def get_the_ball(self):
 
+        aim = False
         if (np.abs(self.ball_angle - self.yaw) > self.angle_tolerance):
-            self.msg.cmd_vel.angular.z = self.ball_angle - self.yaw
+            self.msg.cmd_vel.angular.z = self.ang_scale * (self.ball_angle - self.yaw)
         else:
+            aim = True
             self.msg.cmd_vel.angular.z = 0.0
 
         if self.ball_dist[self.id] > 120:
-            self.msg.cmd_vel.linear.x = 0.3 * np.cos(self.ball_angle - self.yaw)
-            self.msg.cmd_vel.linear.y = 0.3 * np.sin(self.ball_angle - self.yaw)
+            self.msg.cmd_vel.linear.x = self.speed_scale * np.cos(self.ball_angle - self.yaw)
+            self.msg.cmd_vel.linear.y = self.speed_scale * np.sin(self.ball_angle - self.yaw)
         else:
             self.msg.cmd_vel.linear.x = 0.0 * np.cos(self.ball_angle - self.yaw)
             self.msg.cmd_vel.linear.y = 0.0 * np.sin(self.ball_angle - self.yaw)
-            self.msg.dribbler = True
+            self.msg.dribbler = aim
             self.msg.kicker = False
-            self.state = 1
+            if aim:
+                self.state = 1
             # if msg.cmd_vel.angular.z == 0.0:
             #     state = 1
         
@@ -114,45 +167,59 @@ class Player:
 
     
     def drive_to_goal(self):
-
-        if (np.abs(self.goal_angle - self.yaw) > self.angle_tolerance):
+        aim = False
+        if np.abs(self.goal_angle - self.yaw) < self.angle_right_post or np.abs(self.goal_angle - self.yaw) > self.angle_left_post :
             self.msg.cmd_vel.angular.z = self.goal_angle - self.yaw
         else:
+            aim = True
             self.msg.cmd_vel.angular.z = 0.0
 
-        if self.goal_dist[self.id] > 400:
-            self.msg.cmd_vel.linear.x = 0.3 * np.cos(self.goal_angle - self.yaw)
-            self.msg.cmd_vel.linear.y = 0.3 * np.sin(self.goal_angle - self.yaw)
+        if self.goal_dist[self.id] > 600:
+            self.msg.cmd_vel.linear.x = self.speed_scale * np.cos(self.goal_angle - self.yaw)
+            self.msg.cmd_vel.linear.y = self.speed_scale * np.sin(self.goal_angle - self.yaw)
         else:
             self.msg.cmd_vel.linear.x = 0.0 * np.cos(self.goal_angle - self.yaw)
             self.msg.cmd_vel.linear.y = 0.0 * np.sin(self.goal_angle - self.yaw)
-            self.msg.dribbler = False
-            self.msg.kicker = True
+            self.msg.dribbler = not aim
+            self.msg.kicker = aim
 
         self.pub.publish(self.msg)
 
 
     def pass_to_mate(self):
 
+        aim = False
         if (np.abs(self.mate_angle[self.closest_mate] - self.yaw) > self.angle_tolerance):
-            self.msg.cmd_vel.angular.z = self.mate_angle[self.closest_mate] - self.yaw
+            # self.msg.cmd_vel.angular.z = self.mate_angle[self.closest_mate] - self.yaw
+            self.msg.cmd_vel.angular.z = self.ang_scale * (self.mate_angle[self.closest_to_goal] - self.yaw)
         else:
+            aim = True
             self.msg.cmd_vel.angular.z = 0.0
 
-        if self.mate_dist[self.closest_mate] > 500:
-            self.msg.cmd_vel.linear.x = 0.3 * np.cos(self.mate_angle[self.closest_mate] - self.yaw)
-            self.msg.cmd_vel.linear.y = 0.3 * np.sin(self.mate_angle[self.closest_mate] - self.yaw)
+        if self.mate_dist[self.closest_mate] > 1500:
+            self.msg.cmd_vel.linear.x = self.speed_scale * np.cos(self.mate_angle[self.closest_to_goal] - self.yaw)
+            self.msg.cmd_vel.linear.y = self.speed_scale * np.sin(self.mate_angle[self.closest_to_goal] - self.yaw)
         else:
-            self.msg.cmd_vel.linear.x = 0.0 * np.cos(self.mate_angle[self.closest_mate] - self.yaw)
-            self.msg.cmd_vel.linear.y = 0.0 * np.sin(self.mate_angle[self.closest_mate] - self.yaw)
-            self.msg.dribbler = False
-            self.msg.kicker = True
+            self.msg.cmd_vel.linear.x = 0.0 * np.cos(self.mate_angle[self.closest_to_goal] - self.yaw)
+            self.msg.cmd_vel.linear.y = 0.0 * np.sin(self.mate_angle[self.closest_to_goal] - self.yaw)
+            self.msg.dribbler = not aim
+            self.msg.kicker = aim
 
         self.pub.publish(self.msg)
 
     def move_without_ball(self):
-        self.msg.cmd_vel.linear.x = 0.0 * np.cos(self.mate_angle[self.closest_mate] - self.yaw)
-        self.msg.cmd_vel.linear.y = 0.0 * np.sin(self.mate_angle[self.closest_mate] - self.yaw)
+
+        next_x = (min(700, (2000 - self.ball_x)) + self.ball_x) / 2
+        next_y = ((-1 * np.sign(self.ball_y))*1500 + self.ball_y) / 2
+        next_angle = np.arctan2((next_y - self.y), (next_x - self.x))
+        next_dist = np.sqrt(np.abs(self.x - next_x)**2 + np.abs(self.y - next_y)**2)
+
+        if next_dist > 150:
+            self.msg.cmd_vel.linear.x = self.speed_scale * np.cos(next_angle - self.yaw)
+            self.msg.cmd_vel.linear.y = self.speed_scale * np.sin(next_angle - self.yaw)
+        else:
+            self.msg.cmd_vel.linear.x = 0.0 * np.cos(next_angle - self.yaw)
+            self.msg.cmd_vel.linear.y = 0.0 * np.sin(next_angle - self.yaw)
             
         if (np.abs(self.ball_angle - self.yaw) > self.angle_tolerance):
             self.msg.cmd_vel.angular.z = self.ball_angle - self.yaw
